5a2f9407edeaf4ec3f6691b838a67fbc7e3b7fa7
Stephen Bradshaw <ellisdee@Elliss-MacBook-Pro.local>
2024-06-18 17:45:17 -0500

    errors and debugging

--- errors-and-debugging/exercises/DebuggingLogicErrors1.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors1.js
@@ -1,7 +1,9 @@
 // Run this sample code as-is and examine the output. 
 // Should the shuttle have launched? 
-// Did it?
+// Did it? It did not ....
 // Do not worry about fixing the code yet, we will do that in the next series of exercises.
+// okay ...
+
 
 let launchReady = false;
 let fuelLevel = 17000;
