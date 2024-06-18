151965b81be58cc5ced9b0a55502555e2e1e5a27
Stephen Bradshaw <ellisdee@Elliss-MacBook-Pro.local>
2024-06-17 20:24:06 -0500

    boolean-and-conditionals

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -26,10 +26,26 @@ if(astronautCount <= 7
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
 && totalMassKg < maximumMassLimit
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-&& fuelTempCelsius <= -300  && fuelTempCelsius >= -150
+&& fuelTempCelsius >= -300  && fuelTempCelsius <= -150
 // add logic below to verify the fuel level is at 100%
 && fuelLevel === "100%"
 // add logic below to verify the weather status is clear
 && weatherStatus === "clear" ) {
 // Verify shuttle launch can proceed based on above conditions
-console.log("Launch Sequence Ready!") } 
+console.log("All systems are go! Initiating space shuttle launch sequence.");
+console.log("..............................................................");
+console.log("Date:", date);
+console.log("Time:", time);
+console.log("Astronaut Count:", astronautCount);
+console.log("Crew Mass:", crewMassKg, "kg");
+console.log("Fuel Mass:", fuelMassKg, "kg");
+console.log("Shuttle Mass:", shuttleMassKg, " kg");
+console.log("Total Mass:", totalMassKg," kg");
+console.log("Fuel Temperature (Celsius):", fuelTempCelsius,"°C");
+console.log("weatherStatus:", weatherStatus);
+console.log("..............................................................");
+console.log("Have a safe trip astronauts!");
+} else { 
+    console.log("Abort Launch!")
+}
+
