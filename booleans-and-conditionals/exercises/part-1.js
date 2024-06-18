d2a11e7e2969b3b851a775f2f9fb0e2e55dcbbba
Stephen Bradshaw <ellisdee@Elliss-MacBook-Pro.local>
2024-06-18 16:51:41 -0500

    boolean-and-conditionals-excercises

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,5 +1,12 @@
 // Declare and initialize the variables for exercise 1 here:
-
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus	= spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+let fuelLevel = 20000;
+let engineTemperature = 1200;
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
 if (engineIndicatorLight === "green") {
@@ -9,3 +16,51 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+if (crewStatus) {
+   console.log("Crew Ready");
+} else {
+   console.log("Crew Not Ready");
+}
+if (computerStatusCode === 200) {
+   console.log("Please stand by. Computer is rebooting.")
+} else if (computerStatusCode === 400) {
+   console.log("Success! Computer online.")   
+} else {
+   console.log("ALERT: Computer Offline!")
+}
+if (shuttleSpeed > 17500) {
+   console.log("ALERT: Escape velocity reached!")
+} else if (shuttleSpeed < 8000) {
+   console.log("ALERT: Cannot maintain orbit!")
+} else {
+   console.log("Stable speed")
+}
+
+if (fuelLevel < 1000 || engineTemperature > 3500 || engineIndicatorLight === "red blinking"){
+   console.log("ENGINE FAILURE IMMINENT!");
+ } else if (fuelLevel <= 5000 || engineTemperature > 2500){
+   console.log("Check fuel level. Engines running hot.");
+ } else if (fuelLevel > 20000 && engineTemperature <= 2500){
+   console.log("Full tank. Engines good.");
+ } else if (fuelLevel > 10000 && engineTemperature <= 2500){
+   console.log("Fuel level above 50%. Engines good.");
+ } else if (fuelLevel > 5000 && engineTemperature <= 2500){
+   console.log("Fuel level above 25%. Engines good.");
+ } else {
+   console.log("Fuel and engine status pending...");
+ }
+
+ // Create the variable commandOverride, and set it to be true or false. //
+
+ let commandOverrride = false;
+ if (!commandOverrride) {
+   if (fuelLevel > 20000 && !engineIndicatorLight) {
+      console.log("Clear to launch!")
+   } else {
+      console.log("Not clear to launch. Check fuel and engine status.")
+   }
+ } else {
+   console.log("Clear to launch!")
+ }
+
