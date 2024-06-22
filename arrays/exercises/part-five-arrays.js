32c5a904df1ff05ff07e0c5582d28a9e8d5f2903
Stephen Bradshaw <root@Elliss-MacBook-Pro.local>
2024-06-22 10:21:55 -0400

    array exercises

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -3,9 +3,25 @@ let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
 
+console.log(str.split(' '));
+console.log(str.split(''));
+console.log(str.split('e'));
+console.log(`This is the string that's left over after split: ${str}`)
+
+// the purpose is to separate the string into an array, the parameter being the indicator on what to split at
+
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
 
+console.log(arr.join());
+console.log(arr.join('a'));
+console.log(arr.join(''));
+console.log(arr.join(' '));
+
+console.log(arr);
 //3) Do split or join change the original string/array?
+//No
+
 
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+console.log(cargoHold.split(',').sort().join(','));
--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -2,10 +2,41 @@
 
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
 
+let element1 = ['hydrogen', 'H', 1.008];
+let element2 = ['helium', 'He', 4.003];
+let element26 = ['iron', 'Fe', 55.85];
+
+let element3 = ['methane', 'M', 1.558];
+let element5 = ['gold', 'Au', 27.003];
+let element22 = ['copper', 'C', 35.85];
+
 //2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
+
+let table = [];
+let table2 = []
+table.push(element1, element2, element26);
+table2.push(element3, element5, element22);
+console.log(table);
+let tables = []
+tables.push(table, table2)
 
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
 
+console.log(table[0]);
+console.log(table[1][1]);
+
+
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
 
-//5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+console.log(table[0][2]);
+console.log(table[1][0]);
+console.log(table[2][1]);
+
+//5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. 
+// Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+
+console.log(tables[0][0][2]);
+console.log(tables[1][1][0]);
+console.log(tables[1][2][1]);
+
+
