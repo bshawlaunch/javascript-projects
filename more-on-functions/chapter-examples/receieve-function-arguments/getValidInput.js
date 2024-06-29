c1a3433e41e844ec73032990271fce62b8e84d87
Stephen Bradshaw <root@Elliss-MacBook-Pro.local>
2024-06-29 18:48:56 -0400

    more on functions chapter examples

--- more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
+++ more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
@@ -15,7 +15,25 @@ function getValidInput(prompt, isValid) {
 // TODO 1: write a validator 
 // that ensures input starts with "a"
 
+let isValidPrompt = function(inputPrompt) {
+  if (inputPrompt[0].toLowerCase() != "a") {
+    return false;
+  }
+  return true;
+}
+
+console.log(getValidInput('Enter a phrase that begins with the letter "a": ', isValidPrompt));
+
 // TODO 2: write a validator 
 // that ensures input is a vowel
 
+isVowel = function(userInput) {
+  let vowels = ["a","e","i","o","u"];
+  if (vowels.includes(userInput.toLowerCase())) {
+    return true;
+  } 
+  return false;
+}
+
+console.log(getValidInput('Please enter a vowel: ', isVowel));
 // Be sure to test your code!
