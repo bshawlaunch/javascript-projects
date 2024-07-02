4dd4923815bbb88926040ed3edc85280db48ddf9
Stephen Bradshaw <root@Elliss-MacBook-Pro.local>
2024-07-01 21:15:51 -0400

    more on functions studio

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -10,6 +10,16 @@ function findMinValue(arr){
 
 //Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
 
+function sortValues(arr, sortedArray = []) {
+  let max = arr.length
+  for (j = 0; j < max; j++){
+    let val = findMinValue(arr)
+    arr.splice(arr.indexOf(val),1)
+    sortedArray.push(val)
+  }
+  return sortedArray
+}
+
 /*Within the function:
 1) Define a new, empty array to hold the final sorted numbers.
 2) Use the findMinValue function to find the minimum value in the old array.
@@ -23,7 +33,21 @@ function findMinValue(arr){
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
 
+
+function recursionSortValues(arr, sortedArray = []) {
+  if (arr.length === 0) {
+    return sortedArray;
+  }
+  let val = findMinValue(arr);
+  arr.splice(arr.indexOf(val), 1);
+  sortedArray.push(val);
+  return recursionSortValues(arr, sortedArray);
+}
+
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+console.log(sortValues(nums3));
+console.log(recursionSortValues(nums1))
