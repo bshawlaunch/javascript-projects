37f7fc8f6da5976e390fca11060a18c7a9240bf9
Stephen Bradshaw <root@Elliss-MacBook-Pro.local>
2024-08-05 14:06:55 -0500

    dom and even exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -5,6 +5,23 @@ function init () {
 
     // Put your code for the exercises here.
     
+    button.addEventListener("click", function() {
+        paragraph.innerText = "Houston, we have liftoff!";
+    })
+    missionAbort.addEventListener("mouseover", function() {
+        missionAbort.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function() {
+        missionAbort.style.backgroundColor = "";
+    });
+    missionAbort.addEventListener("click", function() {
+        let confirmAbort = confirm("Are you sure you want to abort the mission?");
+        if (confirmAbort) {
+            paragraph.innerText = "Mission aborted! Space shuttle returning home";
+        }
+    })
+
 }
 
 window.addEventListener("load", init);
